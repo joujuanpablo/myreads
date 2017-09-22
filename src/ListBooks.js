f7f0649a4b7b9f11249bd60e4252927b9e6e865e
Jou-Valencia, Juan Pablo (UK - London) <joujuanpablo@gmail.com>
2017-09-22 13:17:04 +0100

    feat: begin to split template code into components
    * With the two screens in mind, begin splitting up the code into components so as to use react-router Router component. Created ListBooks and SearchBooks components
    * Using dummy headings as the component contents for now.
    * Moved template markup into a temporary file.

---
+++ src/ListBooks.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import { Route } from 'react-router-dom';
+
+class ListBooks extends Component {
+    state = {
+
+    }
+
+    render() {
+        return (
+            // TODO: build up the subcomponents from the markup in float.js
+            <h1>List Books Component</h1>
+        )
+    }
+}  
+
+export default ListBooks 
---
+++ src/SearchBooks.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { Route } from 'react-router-dom';
+
+class SearchBooks extends Component {
+    state = {
+
+    }
+    render(){
+        return(
+        // TODO: build up the subcomponents from the markup in float.js
+            <h1>Search Books Component</h1>
+        )
+    }
+}
+
+export default SearchBooks;
