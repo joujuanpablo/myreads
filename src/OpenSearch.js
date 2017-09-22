edacdb8df76b0cf28c55a0db8b0f6ae0c04cb25d
Jou-Valencia, Juan Pablo (UK - London) <joujuanpablo@gmail.com>
2017-09-22 16:20:02 +0100

    feat: enable navigation between the two screens
    * Using the Link component to replace the anchor tags on the open-search and close-search components

--- src/OpenSearch.js
+++ src/OpenSearch.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
 // TODO: use the Link component from react-router-dom to make the button take the user to the search page
 class OpenSearch extends Component {
     render(){
         return(
             <div className="open-search">
-              <a onClick={() => this.setState({ showSearchPage: true })}>Add a book</a>
+              <Link to='/search'>Add a book</Link>
             </div>
         )
     }
--- src/SearchBooks.js
+++ src/SearchBooks.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Route } from 'react-router-dom';
+import { Link } from 'react-router-dom'; 
 
 class SearchBooks extends Component {
     state = {
@@ -9,7 +10,7 @@ class SearchBooks extends Component {
         return(
             <div className="search-books">
             <div className="search-books-bar">
-              <a className="close-search" onClick={() => this.setState({ showSearchPage: false })}>Close</a>
+              <Link to='/' className="close-search">Close</Link>
               <div className="search-books-input-wrapper">
                 {/*
                                           NOTES: The search from BooksAPI is limited to a particular set of search terms.
@@ -20,7 +21,6 @@ class SearchBooks extends Component {
                                           you don't find a specific author or title. Every search is limited by search terms.
                 */}
                 <input type="text" placeholder="Search by title or author"/>
-      
               </div>
             </div>
             <div className="search-books-results">
